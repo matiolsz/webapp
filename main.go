1202d92d252460cc79f554182777a10d24038407
matiolsz <matiolsz97@gmail.com>
2021-11-19 19:26:09 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r http.Request) {
+	
+}
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "hello world")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(fmt.Sprintf("bites written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080",nil)
+}
